5337e34840849fabebdb8f59a65091e9ea5c1fdc
David <thedaviddraw@gmail.com>
2022-11-26 14:08:46 -0600

    documentation

--- firebase/index.js
+++ firebase/index.js
@@ -1,12 +1,18 @@
+//Granting acces to the database
+
 const firebase = require("firebase-admin");
 
 const serviceAccount = require("../key.json");
+
+const mongoose = require("mongoose");
+
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount)
 });
 
 //crear una base de datos(excel)
+
 const db = firebase.firestore();
 
 module.exports = {
